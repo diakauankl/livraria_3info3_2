e3c3df5afe4dae22d42e0bfca90f7757042ded64
diakauankl <kauanluciano673@gmail.com>
2026-03-17 08:54:34 -0300

    feat: Migração do projeto para o novo template

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,7 @@
-from .user import UserRegistrationSerializer, UserSerializer
+from .user import UserRegistrationSerializer
+from .user import UserSerializer
+from .categoria import CategoriaSerializer
+from .editora import EditoraSerializer
+from .autor import AutorSerializer
+from .livro import LivroSerializer
+
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,1 +1,6 @@
-from .user import UserRegistrationView, UserViewSet
+from .user import UserViewSet
+from .user import UserRegistrationView
+from .categoria import CategoriaViewSet
+from .editora import EditoraViewSet
+from .autor import AutorViewSet
+from .livro import LivroViewSet
